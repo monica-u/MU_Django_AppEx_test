1346f017d86e8d55ecb55877270caf7559355017
monica-u <monicau@hawaii.edu>
2022-06-19 21:21:06 -1000

    updated folders

--- greetings/urls.py
+++ greetings/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+
+# urlpatterns = [
+#     path('', views.IndexView.as_view(), name='index'),
+# ]
+
+app_name = 'greetings' # allows using 'greetings:index' for url and reverse_lazy methods
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('new', views.CreateView.as_view(), name='create'),
 ]
--- greetings/views.py
+++ greetings/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from django.urls import reverse_lazy
+from .models import Greeting
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the greetings index.")
+    template_name = 'greetings/index.html'
+    context_object_name = 'greeting_list'
+
+    def get_queryset(self):
+        """Return the all greetings."""
+        return Greeting.objects.all()
+
+class CreateView(generic.edit.CreateView):
+    template_name = 'greetings/create.html'
+    model = Greeting
+    fields = ['message']
+    success_url = reverse_lazy('greetings:index') 
+ 
